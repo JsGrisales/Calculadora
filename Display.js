538922261884aa744975b0af335ec1a2e16c3851
Johan Sebastián Grisales Grisales <js.grisales03@gmail.com>
2022-04-15 15:25:58 -0500

    calculadora-

--- Display.js
+++
@@ -1,8 +0,0 @@
-class Display {
-    constructor(displayValorAnterior, displayValorActual){
-        this.displayValorAnterior = displayValorAnterior
-        this.displayValorActual   = displayValorActual
-        this.calculador = new Calculadora ();
-        this.valorActual
-    }
-}
